e779ae2d7fa0ee77fe82cc605abab5231bd777b6
Bhumit Janghel <bhumit05janghel@gmail.com>
2025-07-07 22:56:45 +0530

    404 error

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    // This handles unknown routes locally by falling back to index.html
+    historyApiFallback: true,
+  },
 })
