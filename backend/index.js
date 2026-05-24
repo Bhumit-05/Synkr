a8456381dff5011ba34e3b810e06cc9dea8ed599
Bhumit Janghel <bhumit05janghel@gmail.com>
2026-05-24 16:47:02 +0530

    CORS update2

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: 'https://synkr-vtpk.onrender.com', 
+  origin: 'https://synkr-xi.vercel.app/', 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
